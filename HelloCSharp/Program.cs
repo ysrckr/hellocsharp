f89d164064f7d05b3a116681fd70fb43f097b902
Yaşar Çakır <yasar@Yasar-MacBook-Pro.local>
2021-02-01 23:39:32 +0300

    first csharp commit

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            double num2 = 1.7;
+            double divide = num1 / num2;
+            Console.WriteLine(divide);
+            Console.Read();
+        }
+    }
+}
